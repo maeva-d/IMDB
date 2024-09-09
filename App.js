a70e62fdbff9c76ff60b988f95a68868d70f7ba1
Maeva DTC <delruemaeva@orange.fr>
2024-09-09 16:42:48 +0200

    first version

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Pressable,
+  SafeAreaView,
+  Image,
+  Platform,
+} from "react-native";
+
+// Permets de connaître la hauteur de la status bar
+import Constants from "expo-constants";
 
 export default function App() {
+  // console.log(Platform);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={[styles.container, styles.androidStyle]}>
+        <Text style={styles.textTitle}>Interstellar</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +27,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#191919",
+    // alignItems: "center",
+    // justifyContent: "center",
+    padding: 24,
+  },
+  androidStyle: {
+    paddingTop: Platform.OS === "android" ? Constants.statusBarHeight : 0,
+  },
+  header: {
+    backgroundColor: "#393939",
+  },
+  textTitle: {
+    color: "white",
+    fontSize: 30,
+  },
+  textInfo: {
+    color: "#A1A1A1",
   },
 });
